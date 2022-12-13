9125c1e465f911f50c5f123fa98bbed69632ceb4
Giovanni De Vanno <giovannidv96@gmail.com>
2022-12-13 20:03:19 +0100

    creato l'array dell'icone

--- app/src/main/java/com/unisannio/gdevanno/cutsomlistview/MainActivity.java
+++ app/src/main/java/com/unisannio/gdevanno/cutsomlistview/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     String fruitList[] = {"Apple","Banana","Apricot","Orange","Water Melon"};
+    int fruitImage[] = {R.drawable.apple, R.drawable.banana, R.drawable.apricot, R.drawable.orangefruit, R.drawable.watermelon};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
